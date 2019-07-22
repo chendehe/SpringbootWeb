07b8f6872a5147927215e518ddc5b9be362e1e03
chendehe <chendehe22@163.com>
2019-07-22 18:59:34 +0800

    加载自定义配置文件

--- demo/src/main/java/com/chendehe/config/RedisConfig.java
+++ demo/src/main/java/com/chendehe/config/RedisConfig.java
@@ -1,34 +1,25 @@
 package com.chendehe.config;
 
-import com.chendehe.util.PropertieUtil;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 /**
  * Redis 配置信息.
  */
-//@Component
-//@ConfigurationProperties(prefix = "jedis")
+@Getter
+@Setter
+@ToString
+@Configuration
+@ConfigurationProperties(prefix = "redis")
+@PropertySource("classpath:redis.properties")
 public class RedisConfig {
 
   private String host;
 
   private int port;
 
-  public String getHost() {
-    return host;
-  }
-
-  public void setHost(String host) {
-    this.host = host;
-  }
-
-  public int getPort() {
-    return port;
-  }
-
-  public void setPort(int port) {
-    this.port = port;
-  }
 }
