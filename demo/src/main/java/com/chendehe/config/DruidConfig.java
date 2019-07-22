d8bb72d120773ed957c8f25fe3996f6c312c872d
chendehe <chendehe22@163.com>
2019-07-22 15:29:27 +0800

    druid 配置

---
+++ demo/src/main/java/com/chendehe/config/DruidConfig.java
@@ -0,0 +1,21 @@
+package com.chendehe.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置druid需要的配置类，引入application.yml文件中以spring.datasource开头的信息
+ * 因此需要在application.yml文件中配置相关信息。
+ *
+ */
+@Configuration
+public class DruidConfig {
+    @Bean  
+    @ConfigurationProperties(prefix = "spring.datasource")  
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    } 
+}
