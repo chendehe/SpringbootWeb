c4105b7998e6d0645cb3f49e24a1988bef8341a6
chendehe <chendehe22@163.com>
2019-07-25 00:48:32 +0800

    调度和异步调度

--- demo/src/main/java/com/chendehe/schedule/SpringSchedule.java
+++ demo/src/main/java/com/chendehe/schedule/SpringSchedule.java
@@ -1,6 +1,7 @@
 package com.chendehe.schedule;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,9 +17,27 @@ public class SpringSchedule {
    * <p>异步的注解@Async，可以指定入参，可以返回Future，也可以指定Executor，</p>
    * <p>不能和类似@PostConstruct对生命周期回调的方法一起使用，需要分两个类来写。</p>
    */
-  //@Scheduled(cron = "*/5 * * * * MON-FRI")
-  public void doSomething() {
+  @Scheduled(fixedDelay = 2000)
+  public void doSomethingSync() {
     System.out.println("doSomething...");
   }
+
+//  @Async
+//  public void doSomethingAsync() {
+//    System.out.println("doSomething...");
+//  }
+//  @Async
+//  public void doSomethingAsync1() {
+//    System.out.println("doSomething1...");
+//  }
+//  @Async
+//  public void doSomethingAsync2() {
+//    System.out.println("doSomething2...");
+//  }
+//  @Async
+//  public void doSomethingAsync3() {
+//    System.out.println("doSomething3...");
+//  }
+
 
 }
