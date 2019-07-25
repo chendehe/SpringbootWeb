1ae653670e8f446cb969a6e1b50789687feb3eb0
chendehe <chendehe22@163.com>
2019-07-25 19:31:20 +0800

    注释

--- demo/src/main/java/com/chendehe/Application.java
+++ demo/src/main/java/com/chendehe/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 @SpringBootApplication
+//配置druid必须加的注解，如果不加，访问页面打不开，
+// filter和servlet、listener之类的需要单独进行注册才能使用，
+// spring boot里面提供了该注解起到注册作用
 @ServletComponentScan
 //需要Redis时放开
 //@ImportAutoConfiguration(RedisAutoConfiguration.class)
