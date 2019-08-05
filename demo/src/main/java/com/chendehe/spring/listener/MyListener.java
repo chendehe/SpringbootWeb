37a3c30bb31cb3833cef09cfcebf9e2cb6d36303
chendehe <chendehe22@163.com>
2019-08-06 00:54:13 +0800

    发布和监听事件

---
+++ demo/src/main/java/com/chendehe/spring/listener/MyListener.java
@@ -0,0 +1,18 @@
+package com.chendehe.spring.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * 监听事件
+ * @author CDH
+ * @since 2019/8/6 0:50
+ */
+@Service
+public class MyListener implements ApplicationListener<ApplicationEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println("MyListener onApplicationEvent: " + event);
+    }
+}
---
+++ demo/src/main/java/com/chendehe/spring/listener/Test.java
@@ -0,0 +1,28 @@
+package com.chendehe.spring.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 测试事务
+ *
+ * @author CDH
+ * @since 2019/7/30 19:32
+ */
+@Configuration
+@ComponentScan("com.chendehe.spring.listener")
+public class Test {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Test.class);
+
+        // 发布事件
+        context.publishEvent(new ApplicationEvent("my event") {
+        });
+        
+        context.close();
+    }
+
+}
